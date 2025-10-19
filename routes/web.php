29545b7e50f279603f83cb1f2318802bd12a2a4b
phamtra12 <trapham24065@gmail.com>
2025-10-19 23:45:54 +0700

    funtion api

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/print/goods-receipt/{receipt}', GoodsReceiptPrintController::class)
         ->name('goods-receipt.print');
 });
+
+Route::get('/chucmung20t10', function () {
+    return view('code20t10.index');
+});
+Route::get('/chucmung20t10/flower', function () {
+    return view('code20t10.index2');
+});
